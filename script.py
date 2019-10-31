3c4be370045e26b005e8b5e80410ba7e23873764
Raphaël Nussbaumer <rafnuss@gmail.com>
2019-10-31 08:22:25 +0100

    update datafiles

--- script.py
+++ script.py
@@ -13,58 +13,89 @@ import json
 #file.write(response.text)
 #file.close()
 
+####################################################
+# PART 1: READ/GET DATA
 # Dowanload Hotspot as Json
 response = requests.request("GET", "https://ebird.org/ws2.0/ref/hotspot/KE", headers={'X-eBirdApiToken': 'vcs68p4j67pt'}, params={"fmt":"json"})
-KE  = json.loads(response.text)
+hotspots  = json.loads(response.text)
 
 # Download specie list
 response = requests.request("GET", "https://ebird.org/barchartData?r=KE", params={"fmt":"json"})
 tmp = json.loads(response.text)
-KE_list = [i['taxon'] for i in tmp['dataRows']]
-# Writing 
-keys = KE_list[0].keys()
-with open('KE_list.csv', 'w') as output_file:
-    dict_writer = csv.DictWriter(output_file, keys)
-    dict_writer.writeheader()
-    dict_writer.writerows(KE_list)
-
-
-# Dowload Barchart KE for all hotspots
-for ke in KE:
-	url = 'http://ebird.org/ebird/barchartData?r={code_loc}&bmo={bmonth}&emo={emonth}&byr={byear}&eyr={eyear}&fmt=json'.format(
-		code_loc=ke['locId'], byear=1900, eyear=2020, bmonth=1, emonth=12)
-	r = requests.get(url)
-	f = open('barchart/barchart_'+ke['locId']+'.json', 'wb')
-	f.write(r.content)
-	f.close()
+species = [i['taxon'] for i in tmp['dataRows']]
 
 # read KE of rht Book '50 Top Bird Sites in Kenya'
 with open('50TopBirdingSites.csv') as f:
     top50 = [{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)]
 
-# Add checklist Add KE to list
+# Dowload Barchart KE for all hotspots
+for hot in hotspots:
+	url = 'http://ebird.org/ebird/barchartData?r={code_loc}&bmo={bmonth}&emo={emonth}&byr={byear}&eyr={eyear}&fmt=json'.format(
+		code_loc=hot['locId'], byear=1900, eyear=2020, bmonth=1, emonth=12)
+	r = requests.get(url)
+	f = open('barchart/barchart_' + hot['locId']+'.json', 'wb')
+	f.write(r.content)
+	f.close()
 
-for ke in KE:
-    tmp = [item for item in top50 if item['code_loc'] == ke['locId']]
+####################################################
+# PART 2: Build the structure
+# hotspots -> species -> data
+
+for hot in hotspots:
+    hot.pop("countryCode", None)
+    hot.pop("subnational1Code", None)
+    hot.pop("locID", None)
+    tmp = [item for item in top50 if item['code_loc'] == hot['locId']]
     if len(tmp)>0:
-        ke['top50'] = tmp[0]['#']
+        hot['top50'] = tmp[0]['#']
     else:
-        ke['top50'] = ''
-    with open('barchart/barchart_'+ke['locId']+'.json') as json_file:
-        BA = json.load(json_file)
-        ke['numSpecies'] = BA['numSpecies'] 
-        if len(BA['dataRows'])>0:
-            ke['numChecklists'] = sum(BA['dataRows'][0]['values_N'])
+        hot['top50'] = ''
+    with open('barchart/barchart_'+hot['locId']+'.json') as json_file:
+        BC = json.load(json_file)
+        hot['numSpecies'] = BC['numSpecies'] 
+        if len(BC['dataRows'])>0:
+            hot['numChecklists'] = sum(BC['dataRows'][0]['values_N'])
+            hot['values_N'] = [ sum(BC['dataRows'][0]['values_N'][i*4:(i*4+1*4)]) for i in range(0,12)] 
         else:
-            ke['numChecklists'] = 0
-        ke['data']=[ [sp['speciesCode'], sp['values']] for sp in BA['dataRows'] ]
+            hot['numChecklists'] = 0
+            hot['values_N'] = []
+        hot['values'] = {}
+        for sp in BC['dataRows']:
+            hot['values'].update({sp['speciesCode']: [ round(sum(sp['values'][i*4:(i*4+1*4)]),2) for i in range(0,12)] })
+        
+# Filter minimum checklist
+hotspots_export = list(filter(lambda x: x['numChecklists'] > 4, hotspots))
+
 
 # Writing the file
-with open('KE_enhanced.json', 'w') as f:
-    json.dump(KE, f)
+tmp = json.dumps(hotspots_export)
+tmp = tmp.replace(' ','').replace('0.0','0')
 
-# filter
+text_file = open("KE_enhanced.json", "w")
+text_file.write(tmp)
+text_file.close()
 
+####################################################
+# PART 3: Export species list
+
+# Remove unsued field
+for sp in species:
+    sp.pop('commonName', None)
+    sp.pop('reportAsSpeciesCode', None)
+    sp.pop('reportAsCategory', None)
+    sp.pop('showSpeciesPageLink', None)
+
+# Keep only field with specie in the data
+for hot in hotspots_export:
+    tmp += list(hot['values'].keys())
+
+myset = set(tmp)
+
+species_export = list(filter(lambda x: x['speciesCode'] in myset, species))
+
+# Export
+with open('KE_species.json', 'w') as f:
+    json.dump(species_export, f)
 
 
 
