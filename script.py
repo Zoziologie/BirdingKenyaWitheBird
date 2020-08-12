9d1053a8ddf35496127c0c6548b077d100c4dd27
Raphaël Nussbaumer <rafnuss@gmail.com>
2020-08-12 14:20:59 +0200

    update

--- script.py
+++ script.py
@@ -2,77 +2,101 @@
 import csv
 import os
 import requests
-import unidecode
-import time
+#import unidecode
+#import time
 import json
 import numpy
 
 
+# Define region
+re = 'KE'
+re = 'ES'
+
 # download list of hotspot for region
-#response = requests.request("GET", "https://ebird.org/ws2.0/ref/hotspot/KE", headers={'X-eBirdApiToken': 'vcs68p4j67pt'}, params={"fmt":"csv"})
-#file = open("KE/KE.csv", "w")
-#file.write(response.text)
-#file.close()
+response = requests.request("GET", "https://ebird.org/ws2.0/ref/hotspot/"+re, headers={'X-eBirdApiToken': 'vcs68p4j67pt'}, params={"fmt":"csv"})
+file = open(re+"/hotspots.csv", "w")
+file.write(response.text)
+file.close()
 
 ####################################################
 # PART 1: READ/GET DATA
 # Dowanload Hotspot as Json
-response = requests.request("GET", "https://ebird.org/ws2.0/ref/hotspot/KE", headers={'X-eBirdApiToken': 'vcs68p4j67pt'}, params={"fmt":"json"})
+response = requests.request("GET", "https://ebird.org/ws2.0/ref/hotspot/"+re, headers={'X-eBirdApiToken': 'vcs68p4j67pt'}, params={"fmt":"json"})
 hotspots  = json.loads(response.text)
 
 # Download specie list
-response = requests.request("GET", "https://ebird.org/barchartData?r=KE", params={"fmt":"json"})
+response = requests.request("GET", "https://ebird.org/barchartData?r="+re, params={"fmt":"json"})
 tmp = json.loads(response.text)
 species = [i['taxon'] for i in tmp['dataRows']]
 
 # read KE of rht Book '50 Top Bird Sites in Kenya'
-with open('50TopBirdingSites.csv') as f:
-    top50 = [{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)]
+if re=="KE":
+    with open('50TopBirdingSites.csv') as f:
+        top50 = [{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)]
 
 # Dowload Barchart KE for all hotspots
+# Filter for hotspot having at leat 80 species recorded.
 for hot in hotspots:
-	url = 'http://ebird.org/ebird/barchartData?r={code_loc}&bmo={bmonth}&emo={emonth}&byr={byear}&eyr={eyear}&fmt=json'.format(
-		code_loc=hot['locId'], byear=1900, eyear=2020, bmonth=1, emonth=12)
-	r = requests.get(url)
-	f = open('barchart/barchart_' + hot['locId']+'.json', 'wb')
-	f.write(r.content)
-	f.close()
+    if 'numSpeciesAllTime' in hot:
+        if hot['numSpeciesAllTime']>80:
+        	url = 'http://ebird.org/ebird/barchartData?r={code_loc}&bmo={bmonth}&emo={emonth}&byr={byear}&eyr={eyear}&fmt=json'.format(
+        		code_loc=hot['locId'], byear=1900, eyear=2020, bmonth=1, emonth=12)
+        	r = requests.get(url)
+        	f = open(re+'/barchart/barchart_' + hot['locId']+'.json', 'wb')
+        	f.write(r.content)
+        	f.close()
 
 ####################################################
 # PART 2: Build the structure
 # hotspots -> species -> data
 
-for hot in hotspots:
-    hot.pop("countryCode", None)
-    # hot.pop("subnational1Code", None)
-    hot.pop("locID", None)
-    tmp = [item for item in top50 if item['code_loc'] == hot['locId']]
-    if len(tmp)>0:
-        hot['top50'] = tmp[0]['#']
-    else:
-        hot['top50'] = ''
-    with open('barchart/barchart_'+hot['locId']+'.json') as json_file:
+hotspots_enhanced = []
+
+for bc in os.listdir(path=re+'/barchart/'):
+    with open(re+'/barchart/'+bc) as json_file:
         BC = json.load(json_file)
+        
+        hot = [item for item in hotspots if item["locId"] == bc.replace('.json','').replace('barchart_','')][0]
+        
+        # hot.pop("countryCode", None)
+        # hot.pop("subnational1Code", None)
+        hot.pop("locID", None)
+        
+        if re=="KE":
+            tmp = [item for item in top50 if item['code_loc'] == hot['locId']]
+            if len(tmp)>0:
+                hot['top50'] = tmp[0]['#']
+            else:
+                hot['top50'] = ''
+        
         hot['numSpecies'] = BC['numSpecies'] 
+        
         if len(BC['dataRows'])>0:
             hot['numChecklists'] = sum(BC['dataRows'][0]['values_N'])
             hot['values_N'] = [ sum(BC['dataRows'][0]['values_N'][i*4:(i*4+1*4)]) for i in range(0,12)] 
         else:
             hot['numChecklists'] = 0
             hot['values_N'] = []
+        
         hot['values'] = {}
+        
         for sp in BC['dataRows']:
             sp['values'] = numpy.multiply(sp['values'], BC['dataRows'][0]['values_N'])
             hot['values'].update({sp['speciesCode']: [ round(sum(sp['values'][i*4:(i*4+1*4)]),2) for i in range(0,12)] })
         
+        hotspots_enhanced.append(hot)
+        
+    
+            
+    
 # Filter minimum checklist
-hotspots_export = list(filter(lambda x: x['numChecklists'] > 4, hotspots))
+hotspots_enhanced = list(filter(lambda x: x['numChecklists'] > 4, hotspots_enhanced))
 
 # Writing the file
-tmp = json.dumps(hotspots_export)
+tmp = json.dumps(hotspots_enhanced)
 tmp = tmp.replace(', ',',').replace(',0.0,',',0,').replace('.0,',',').replace('.0]',']')
 
-text_file = open("KE_enhanced.json", "w")
+text_file = open(re+"/hotspots_enhanced.json", "w")
 text_file.write(tmp)
 text_file.close()
 
@@ -141,7 +165,7 @@ myset = set(tmp)
 species_export = list(filter(lambda x: x['speciesCode'] in myset, species))
 
 # Export
-with open('KE_species.json', 'w') as f:
+with open('species.json', 'w') as f:
     json.dump(species_export, f)
 
 
